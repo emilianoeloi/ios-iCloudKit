c80d34b8b97f78a6af839b4f2e4e269cba84cd46
Emiliano S. Barbosa <emilianoeloi@gmail.com>
2016-05-07 13:36:51 -0300

    Initial Commit

---
+++ gameOfThrones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gameOfThrones
+//
+//  Created by Emiliano on 5/7/16.
+//  Copyright © 2016 Emiliano Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
